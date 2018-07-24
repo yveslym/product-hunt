28f176b318137a1565f330d5f9e5081687fa5d8c
Yves Songolo <yveslym@Yvess-MacBook-Pro.local>
2018-07-23 22:21:56 -0700

    Initial commit

---
+++ product-hunt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  product-hunt
+//
+//  Created by Yves Songolo on 7/23/18.
+//  Copyright © 2018 Yves Songolo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
